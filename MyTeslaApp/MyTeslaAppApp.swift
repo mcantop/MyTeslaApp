ccc2a9160a6ef7ca17e9518d16f09de4d4417b81
mcantop <maciejcantop@icloud.com>
2022-09-03 01:28:30 +0200

    Initial Commit

---
+++ MyTeslaApp/MyTeslaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTeslaAppApp.swift
+//  MyTeslaApp
+//
+//  Created by Maciej on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyTeslaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
